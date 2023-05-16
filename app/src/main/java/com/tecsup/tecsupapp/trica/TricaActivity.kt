7ce4ab8c632af657d5ec7c79dd4964fa49239307
MasterCard31 <carlos.mendoza.s@tecsup.edu.pe>
2023-05-16 16:50:16 -0500

    Añadi condicionales al TricaActivity.kt

--- app/src/main/java/com/tecsup/tecsupapp/trica/TricaActivity.kt
+++ app/src/main/java/com/tecsup/tecsupapp/trica/TricaActivity.kt
@@ -1,11 +1,15 @@
 package com.tecsup.tecsupapp.trica
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProvider
+import com.tecsup.tecsupapp.DashboardActivity
 import com.tecsup.tecsupapp.R
 
 class TricaActivity : AppCompatActivity(){
@@ -22,7 +26,7 @@ class TricaActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trica)
 
-        viewModel = ViewModelProvider(this).get(TricaViewModel::class.java)
+        viewModel = ViewModelProvider(this)[TricaViewModel::class.java]
 
         val practicaTeo1 = findViewById<EditText>(R.id.practicaTeo1)
         val practicaTeo2 = findViewById<EditText>(R.id.practicaTeo2)
@@ -30,21 +34,51 @@ class TricaActivity : AppCompatActivity(){
         val practicaLab2 = findViewById<EditText>(R.id.practicaLab2)
         val practicaLab3 = findViewById<EditText>(R.id.practicaLab3)
         val practicaLab4 = findViewById<EditText>(R.id.practicaLab4)
-        val resultadoEditText = findViewById<TextView>(R.id.resultadoEditText)
+        val resultadoEditText = findViewById<TextView>(R.id.resultadoTextView)
 
         val btnCalcularPromedio = findViewById<Button>(R.id.btnCalcularPromedio)
+
         btnCalcularPromedio.setOnClickListener {
-            val number1 = practicaTeo1.text.toString()
-            val number2 = practicaTeo2.text.toString()
-            val number3 = practicaLab1.text.toString()
-            val number4 = practicaLab2.text.toString()
-            val number5 = practicaLab3.text.toString()
-            val number6 = practicaLab4.text.toString()
+            val pTeo1 = practicaTeo1.text.toString()
+            val pTeo2 = practicaTeo2.text.toString()
+            val pLab1 = practicaLab1.text.toString()
+            val pLab2 = practicaLab2.text.toString()
+            val pLab3 = practicaLab3.text.toString()
+            val pLab4 = practicaLab4.text.toString()
 
-            val result = viewModel.sumNumbers(number1, number2, number3, number4, number5, number6)
-            resultadoEditText.text = "El resultado es: $result"
+            val errorFields = ArrayList<String>()
+
+            if (pTeo1.isBlank()) {
+                errorFields.add("Práctica 1")
+            }
+            else if (pTeo2.isBlank()) {
+                errorFields.add("Práctica 2")
+            }
+            else if (pLab1.isBlank()) {
+                errorFields.add("Práctica 1")
+            }
+            else if (pLab2.isBlank()) {
+                errorFields.add("Práctica 2")
+            }
+            else if (pLab3.isBlank()) {
+                errorFields.add("Práctica 3")
+            }
+            else if (pLab4.isBlank()) {
+                errorFields.add("Práctica 4")
+            }
+            if (errorFields.isNotEmpty()) {
+                val errorMessage = "${errorFields.joinToString(", ")}"
+                showErrorMessage(errorMessage)
+            }else {
+                try {
+                    val result = viewModel.sumNumbers(pTeo1, pTeo2, pLab1, pLab2, pLab3, pLab4)
+                    resultadoEditText.text = "  $result"
+                } catch (e: Exception) {
+                    val errorMessage = "Error en la suma: ${e.message}"
+                    showErrorMessage(errorMessage)
+                }
+            }
         }
-
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -52,4 +86,33 @@ class TricaActivity : AppCompatActivity(){
         return true
     }
 
+    private fun showErrorMessage(message: String) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Ingrese la Nota")
+        builder.setMessage(message)
+        builder.setPositiveButton("Aceptar", null)
+        builder.show()
+    }
+
 }
+
+
+/*var result = 0
+
+    if ( pTeo1 < 0.toString() && pTeo1 > 20.toString() || pTeo2 < 0.toString() && pTeo2 > 20.toString() || pLab1 < 0.toString() && pLab1 > 20.toString() || pLab2 < 0.toString() && pLab2 > 20.toString() || pLab3 < 0.toString() && pLab3 > 20.toString() || pLab4 < 0.toString() && pLab4 > 20.toString()){
+        tricaStringError.value = "Ingrese una nota válida"
+    }else if(pTeo1.isEmpty()){
+        tricaStringError.value = "Ingrese la Práctica 1"
+    }else if(pTeo2.isEmpty()){
+        tricaStringError.value = "Ingrese la Práctica 2"
+    }else if(pLab1.isEmpty()){
+        tricaStringError.value = "Ingrese la Práctica 1"
+    }else if(pLab2.isEmpty()){
+        tricaStringError.value = "Ingrese la Práctica 2"
+    }else if(pLab3.isEmpty()){
+        tricaStringError.value = "Ingrese la Práctica 3"
+    }else if(pLab4.isEmpty()){
+        tricaStringError.value = "Ingrese la Práctica 4"
+    }else{
+        result = viewModel.sumNumbers(pTeo1, pTeo2, pLab1, pLab2, pLab3, pLab4).toInt()
+    }*/
--- app/src/main/java/com/tecsup/tecsupapp/trica/TricaViewModel.kt
+++ app/src/main/java/com/tecsup/tecsupapp/trica/TricaViewModel.kt
@@ -3,8 +3,10 @@ package com.tecsup.tecsupapp.trica
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+class TricaViewModel : ViewModel() {
 
-class TricaViewModel : ViewModel(){
+    // MUtableLiveData
+    val tricaStringError = MutableLiveData<String>()
 
     fun sumNumbers(
         practicaTeo1: String,
@@ -13,20 +15,38 @@ class TricaViewModel : ViewModel(){
         practicaLab2: String,
         practicaLab3: String,
         practicaLab4: String
+
     ): Double {
-        val pTeo1 = practicaTeo1.toIntOrNull() ?: 0
-        val pTeo2 = practicaTeo2.toIntOrNull() ?: 0
-        val pLab1 = practicaLab1.toIntOrNull() ?: 0
-        val pLab2 = practicaLab2.toIntOrNull() ?: 0
-        val pLab3 = practicaLab3.toIntOrNull() ?: 0
-        val pLab4 = practicaLab4.toIntOrNull() ?: 0
 
-        return (pTeo1 + pTeo2)*0.30 + (pLab1 + pLab2 + pLab3 + pLab4)*0.70
+        if (practicaTeo1.isEmpty()) {
+            tricaStringError.value = "Ingrese una nota válida"
+        } else if (practicaTeo2.isEmpty()) {
+            tricaStringError.value = "Ingrese la Práctica 1"
+        } else if (practicaLab1.isEmpty()) {
+            tricaStringError.value = "Ingrese la Práctica 1"
+        } else if (practicaLab2.isEmpty()) {
+            tricaStringError.value = "Ingrese la Práctica 2"
+        } else if (practicaLab3.isEmpty()) {
+            tricaStringError.value = "Ingrese la Práctica 3"
+        } else if (practicaLab4.isEmpty()) {
+            tricaStringError.value = "Ingrese la Práctica 4"
+        }
+
+
+        val pTeo1 = practicaTeo1.toDoubleOrNull() ?: 0.0
+        val pTeo2 = practicaTeo2.toDoubleOrNull() ?: 0.0
+        val pLab1 = practicaLab1.toDoubleOrNull() ?: 0.0
+        val pLab2 = practicaLab2.toDoubleOrNull() ?: 0.0
+        val pLab3 = practicaLab3.toDoubleOrNull() ?: 0.0
+        val pLab4 = practicaLab4.toDoubleOrNull() ?: 0.0
+
+        if (pTeo1 < 0 && pTeo1 > 20 || pTeo2 < 0 && pTeo2 > 20 || pLab1 < 0 && pLab1 > 20 || pLab2 < 0 && pLab2 > 20 || pLab3 < 0 && pLab3 > 20 || pLab4 < 0 && pLab4 > 20) {
+            tricaStringError.value = "Ingrese una nota válida"
+        }
+
+        return (pTeo1 + pTeo2)/2*0.40 + (pLab1 + pLab2 + pLab3 + pLab4)/4*0.60
+
     }
-
-
-
-
-
 }
+
 
