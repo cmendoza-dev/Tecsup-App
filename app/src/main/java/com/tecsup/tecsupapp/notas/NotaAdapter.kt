a7de0688f6bf7fbf072a685c92cf9da9eab2ffd7
MasterCard31 <carlos.mendoza.s@tecsup.edu.pe>
2023-05-20 11:11:44 -0500

    Base de Datos SqlLite

---
+++ app/src/main/java/com/tecsup/tecsupapp/notas/NotaAdapter.kt
@@ -0,0 +1,23 @@
+package com.tecsup.tecsupapp.notas
+
+import android.view.ViewGroup
+import android.widget.AdapterView.OnItemClickListener
+import androidx.recyclerview.widget.RecyclerView
+
+class NotaAdapter (val itemClick : OnItemClickListener) : RecyclerView.Adapter<NotaViewHolder>() {
+
+
+
+    override fun getItemCount(): Int {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotaViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: NotaViewHolder, position: Int) {
+
+    }
+
+}
---
+++ app/src/main/java/com/tecsup/tecsupapp/notas/NotaViewHolder.kt
@@ -0,0 +1,31 @@
+package com.tecsup.tecsupapp.notas
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.tecsup.tecsupapp.R
+
+class NotaViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
+    RecyclerView.ViewHolder(inflater.inflate(R.layout.item_nota, parent, false)){
+
+        private var textDate : TextView? = null
+        private var textTitle : TextView? = null
+        private var textDescription : TextView? = null
+
+        init {
+            textDate = itemView.findViewById(R.id.textDate)
+            textTitle = itemView.findViewById(R.id.textTitle)
+            textDescription = itemView.findViewById(R.id.textDescription)
+        }
+
+        fun bind(nota : Nota) {
+            textDate?.text = nota.fecha
+            textTitle?.text = nota.titulo
+            textDescription?.text = nota.contenido
+        }
+}
+
+
+
+
